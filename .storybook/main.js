d6eaf43a4052b988831c733903df3c800ee50dff
Thomas Allmer <d4kmor@gmail.com>
2020-04-13 19:29:27 +0200

    chore: update to latest version of @open-wc/demoing-storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,10 @@ module.exports = {
     '../README.md',
     '../docs/**/*.md',
   ],
+  addons: [
+    'storybook-prebuilt/addon-docs/register.js',
+    'storybook-prebuilt/addon-viewport/register.js',
+  ],
   esDevServer: {
     nodeResolve: true,
     watch: true,
